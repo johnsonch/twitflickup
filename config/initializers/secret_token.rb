3312041ce7f0c17a379710f04042faac436daedb
Chris Johnson <chris@johnsonch.com>
2011-10-24 07:02:59 -0500

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twitflickup::Application.config.secret_token = 'ae415c82c06b5c21431e9ff7c35ffb7a0c814668e8154c49f8e2808d4ed195384948261ebe202a73a81683615c11d16acf73c6a50f0c13b0eeba08bbf30cd9bb'
